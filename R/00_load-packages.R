fe6b2ccb7ecb83590c68e3dedff5850ae19a55a0
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-09-23 08:25:11 -0400

    updated packages

--- R/00_load-packages.R
+++ R/00_load-packages.R
@@ -3,7 +3,9 @@
 # 00 "I need to install them!" ----
 # run the following lines if you need to install the packages for the first time
 
-workshop.packages <- c("tidyverse", "janitor", "here", "readxl", "knitr", "plotly", "kableExtra", "broom")
+workshop.packages <- c("tidyverse", "janitor", "here", "readxl", 
+                       "knitr", "plotly", "kableExtra", "broom",
+                       "rmarkdown")
 install.packages(workshop.packages)
 
 # 01 "I'm good. Just need to load them to use for the Workshop" ----
@@ -15,3 +17,4 @@ library(knitr)
 library(plotly)
 library(kableExtra)  # https://cran.r-project.org/web/packages/kableExtra/vignettes/awesome_table_in_html.html 
 library(broom)
+library(rmarkdown)
